0aaf2206b4aa25b2b2edbb46939c7c83c277c6f7
Kristina <kristina.rastas@gmail.com>
2017-01-06 16:29:38 +0200

    Klass

--- src/Projekt.java
+++ src/Projekt.java
@@ -20,15 +20,14 @@ import java.util.Collections;
 public class Projekt extends Application {
     public ArrayList<ImageView> jupid = new ArrayList();
     public ArrayList<ImageView> korras = new ArrayList<>();
-    public ArrayList<ImageView> kontroll = new ArrayList<>();
 
     Stage puzzle = new Stage();
     ImageView pilt1;
-    int asukoht1;
-    int asukoht2;
+    public static int asukoht1;
+    public static int asukoht2;
 
-    int valgex = 2;
-    int valgey = 2;
+    public static int valgex = 2;
+    public static int valgey = 2;
     GridPane grid = new GridPane();
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -63,17 +62,13 @@ public class Projekt extends Application {
         grid.setOnMouseClicked(event -> {  //klikkides vahetab nuppe
             Node jupp = (Node)(event.getTarget());
             pilt1 = (ImageView)(event.getTarget());
-            swap(jupp);
+            new swap(jupp, grid, pilt1, jupid, korras, puzzle);
         });
-
-
-
 
 
        ImageView viimane = jupid.remove(8); //kustutab viimase jupi
         Collections.shuffle(jupid);
         jupid.add(null);  //paneb tühja asemele
-        kontroll = jupid;
         int z =0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -98,44 +93,6 @@ public class Projekt extends Application {
                 puzzle.setScene(piltStseen);
 
         });
-
-    }
-
-    public void swap(Node jupp) {
-        int y = grid.getRowIndex(jupp);
-        int x = grid.getColumnIndex(jupp);
-        System.out.println(jupp);
-        if ((x + 1 == valgex && y == valgey) || //kui valge on paremal
-                (x == valgex && y + 1 == valgey) || //kui valge on all
-                    (x - 1 == valgex && y == valgey) || //kui valge on vasakul
-                        (x == valgex && y - 1 == valgey)){
-            //System.out.println("valge on kõrval");
-
-            asukoht2 = kontroll.indexOf(null);
-            asukoht1 = kontroll.indexOf(pilt1);
-            kontroll.set(asukoht2, pilt1);
-            kontroll.set(asukoht1, null);
-
-            grid.getChildren().remove(jupp);
-            grid.add(jupp, valgex, valgey);
-            Rectangle rect = new Rectangle(100, 100);
-            rect.setOpacity(0);
-            grid.add(rect, x, y);
-            valgex = x;
-            valgey = y;
-            asukoht2 = asukoht1; //muudab valge asukohaks pildi eelmise asukoha
-
-            if (kontroll.equals(korras)){
-
-                Image image1 = new Image("Pilt/Pingviinid.jpg" );
-                ImageView pic = new ImageView();
-                pic.setImage(image1);
-                VBox vbox2 = new VBox();
-                vbox2.getChildren().add(pic);
-                Scene korraspilt = new Scene(vbox2, 300, 300);
-                puzzle.setScene(korraspilt);
-            }
-        }
 
     }
 }
---
+++ src/swap.java
@@ -0,0 +1,53 @@
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kristina on 6.01.2017.
+ */
+public class swap {
+    public swap(Node jupp, GridPane grid, ImageView pilt1, ArrayList<ImageView> jupid, ArrayList<ImageView> korras, Stage puzzle) {
+
+        int y = grid.getRowIndex(jupp);
+        int x = grid.getColumnIndex(jupp);
+        System.out.println(jupp);
+        if ((x + 1 == Projekt.valgex && y == Projekt.valgey) || //kui valge on paremal
+                (x == Projekt.valgex && y + 1 == Projekt.valgey) || //kui valge on all
+                (x - 1 == Projekt.valgex && y == Projekt.valgey) || //kui valge on vasakul
+                (x == Projekt.valgex && y - 1 == Projekt.valgey)) {
+            //System.out.println("valge on kõrval");
+
+            Projekt.asukoht2 = jupid.indexOf(null);
+            Projekt.asukoht1 = jupid.indexOf(pilt1);
+            jupid.set(Projekt.asukoht2, pilt1);
+            jupid.set(Projekt.asukoht1, null);
+
+            grid.getChildren().remove(jupp);
+            grid.add(jupp, Projekt.valgex, Projekt.valgey);
+            Rectangle rect = new Rectangle(100, 100);
+            rect.setOpacity(0);
+            grid.add(rect, x, y);
+            Projekt.valgex = x;
+            Projekt.valgey = y;
+            Projekt.asukoht2 = Projekt.asukoht1; //muudab valge asukohaks pildi eelmise asukoha
+
+            if (jupid.equals(korras)) {
+
+                Image image1 = new Image("Pilt/Pingviinid.jpg");
+                ImageView pic = new ImageView();
+                pic.setImage(image1);
+                VBox vbox2 = new VBox();
+                vbox2.getChildren().add(pic);
+                Scene korraspilt = new Scene(vbox2, 300, 300);
+                puzzle.setScene(korraspilt);
+            }
+        }
+    }
+}
