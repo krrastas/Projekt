67e330954cce221f99b952d57ba3e71842bee9d7
Kristina <kristina.rastas@gmail.com>
2017-01-06 15:55:23 +0200

    Lõpustseeniga

--- src/Projekt.java
+++ src/Projekt.java
@@ -2,44 +2,41 @@
  * Created by Kristina on 18.11.2016.
  */
 import javafx.application.Application;
-import javafx.collections.transformation.SortedList;
-import javafx.event.EventType;
-import javafx.geometry.HPos;
 import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import sun.swing.MenuItemLayoutHelper;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Projekt extends Application {
-    ArrayList<ImageView> jupid = new ArrayList();
+    public ArrayList<ImageView> jupid = new ArrayList();
+    public ArrayList<ImageView> korras = new ArrayList<>();
+    public ArrayList<ImageView> kontroll = new ArrayList<>();
 
+    Stage puzzle = new Stage();
+    ImageView pilt1;
+    int asukoht1;
+    int asukoht2;
 
     int valgex = 2;
     int valgey = 2;
-    GridPane root = new GridPane();
+    GridPane grid = new GridPane();
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         VBox vbox = new VBox();
         Scene login = new Scene(vbox, 300,300);
-        primaryStage.setScene(login);
-        primaryStage.show();
+        puzzle.setScene(login);
+        puzzle.show();
         Label pealkiri = new Label("Puzzle mäng");
         Button submitButton = new Button("Alusta");
         vbox.getChildren().addAll(pealkiri, submitButton);
@@ -55,15 +52,17 @@ public class Projekt extends Application {
                 ImageView piltobjekt = new ImageView();
                 piltobjekt.setImage(newImage);
                 jupid.add(piltobjekt);
+                korras.add(piltobjekt);
 
 
             }
         }
+        korras.remove(8);
+        korras.add(null);
 
-        System.out.println(jupid);
-
-        root.setOnMouseClicked(event -> {  //klikkides vahetab nuppe
+        grid.setOnMouseClicked(event -> {  //klikkides vahetab nuppe
             Node jupp = (Node)(event.getTarget());
+            pilt1 = (ImageView)(event.getTarget());
             swap(jupp);
         });
 
@@ -74,50 +73,68 @@ public class Projekt extends Application {
        ImageView viimane = jupid.remove(8); //kustutab viimase jupi
         Collections.shuffle(jupid);
         jupid.add(null);  //paneb tühja asemele
-
+        kontroll = jupid;
         int z =0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 ImageView pilt = jupid.get(z);
                 if (pilt == null) {
-                    root.add(new Rectangle(100, 100), j, i);
+                    grid.add(new Rectangle(100, 100), j, i);
                 }
                 else {
-                    root.add(pilt, j, i);
+                    grid.add(pilt, j, i);
                 }
                 z = z+1;
             }
 
         }
         System.out.println(jupid);
-       root.getChildren().get(8).setOpacity(0);
+       grid.getChildren().get(8).setOpacity(0);
         //System.out.println(jupid);
         //root.getChildren().add(imageView);
-        Scene piltStseen = new Scene(root, 300, 300);
+        Scene piltStseen = new Scene(grid, 300, 300);
 
             submitButton.setOnAction(event -> {
-                primaryStage.setScene(piltStseen);
+                puzzle.setScene(piltStseen);
 
         });
 
     }
 
     public void swap(Node jupp) {
-        int y = root.getRowIndex(jupp);
-        int x = root.getColumnIndex(jupp);
+        int y = grid.getRowIndex(jupp);
+        int x = grid.getColumnIndex(jupp);
         System.out.println(jupp);
         if ((x + 1 == valgex && y == valgey) || //kui valge on paremal
                 (x == valgex && y + 1 == valgey) || //kui valge on all
                     (x - 1 == valgex && y == valgey) || //kui valge on vasakul
                         (x == valgex && y - 1 == valgey)){
             //System.out.println("valge on kõrval");
-            root.getChildren().remove(jupp);
-            root.add(jupp, valgex, valgey);
+
+            asukoht2 = kontroll.indexOf(null);
+            asukoht1 = kontroll.indexOf(pilt1);
+            kontroll.set(asukoht2, pilt1);
+            kontroll.set(asukoht1, null);
+
+            grid.getChildren().remove(jupp);
+            grid.add(jupp, valgex, valgey);
             Rectangle rect = new Rectangle(100, 100);
             rect.setOpacity(0);
-            root.add(rect, x, y);
+            grid.add(rect, x, y);
             valgex = x;
             valgey = y;
+            asukoht2 = asukoht1; //muudab valge asukohaks pildi eelmise asukoha
+
+            if (kontroll.equals(korras)){
+
+                Image image1 = new Image("Pilt/Pingviinid.jpg" );
+                ImageView pic = new ImageView();
+                pic.setImage(image1);
+                VBox vbox2 = new VBox();
+                vbox2.getChildren().add(pic);
+                Scene korraspilt = new Scene(vbox2, 300, 300);
+                puzzle.setScene(korraspilt);
+            }
         }
 
     }
